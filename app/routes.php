7baf4fa1d69e1a8f061c1a87de9836c698c43b2b
Marcos Andrade <cin_mandrade@uolinc.com>
2020-03-16 16:42:05 -0300

    Estrutura angular

--- app/routes.php
+++ app/routes.php
@@ -6,12 +6,16 @@ use App\Application\Actions\User\ViewUserAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
-use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;  
+use Slim\Views\PhpRenderer;
+
+$container->view = new PhpRenderer("../public/");
 
 return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
+       
+        
+        return $this->view->render($response, 'index.html');
     });
 
     $app->group('/users', function (Group $group) {
